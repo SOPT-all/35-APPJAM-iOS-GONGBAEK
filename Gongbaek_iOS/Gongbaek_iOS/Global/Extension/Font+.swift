723f23c0c8d652be1b0fc3fdae5d9af6887108f7
김나연 <nanalovesbap@gmail.com>
2025-01-08 05:21:42 +0900

    [Setting] 기본 폴더링 구조 세팅

---
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Extension/Font+.swift
@@ -0,0 +1,8 @@
+//
+//  Font+.swift
+//  Gongbaek_iOS
+//
+//  Created by 김나연 on 1/8/25.
+//
+
+import SwiftUI
---
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Literal/String.swift
@@ -0,0 +1,8 @@
+//
+//  String.swift
+//  Gongbaek_iOS
+//
+//  Created by 김나연 on 1/8/25.
+//
+
+import Foundation
