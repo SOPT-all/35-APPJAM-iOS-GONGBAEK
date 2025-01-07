06861dfbf411caf345d9c9c0ebf71679af261bf2
김나연 <nanalovesbap@gmail.com>
2025-01-08 03:37:33 +0900

    [Setting] 프로젝트 세팅

---
+++ Gongbaek_iOS/Gongbaek_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gongbaek_iOS
+//
+//  Created by 김나연 on 1/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
