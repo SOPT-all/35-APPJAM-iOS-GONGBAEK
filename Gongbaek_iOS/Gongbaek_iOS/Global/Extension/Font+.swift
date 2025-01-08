aef979ad914c5d99999eb53b0f29fdfd1e66cacf
hee <heheeun13@gmail.com>
2025-01-09 01:35:15 +0900

    [Setting/#1] Font 기본 Setting 처리 - fontWeight, Size

--- Gongbaek_iOS/Gongbaek_iOS/Global/Extension/Font+.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Extension/Font+.swift
@@ -9,21 +9,40 @@ import SwiftUI
 
 extension Font {
     
-    // Bold
-    static let pretendardBold28: Font = .custom("Pretendard-Bold", size: 28)
-    static let pretendardBold24: Font = .custom("Pretendard-Bold", size: 24)
-    static let pretendardBold18: Font = .custom("Pretendard-Bold", size: 18)
-    static let pretendardBold14: Font = .custom("Pretendard-Bold", size: 14)
+    // head
+    static let head1_b_30: Font = .custom("Pretendard-Bold", size: 30)
+    static let head2_b_24: Font = .custom("Pretendard-Bold", size: 24)
+    static let head2_m_24: Font = .custom("Pretendard-Medium", size: 24)
+    static let head2_r_24: Font = .custom("Pretendard-Regular", size: 24)
     
-    // SemiBold
-    static let pretendardSemiBold16: Font = .custom("Pretendard-SemiBold", size: 16)
+    // title
+    static let title1_b_20: Font = .custom("Pretendard-Bold", size: 20)
+    static let title1_m_20: Font = .custom("Pretendard-Medium", size: 20)
+    static let title1_r_20: Font = .custom("Pretendard-Regular", size: 20)
     
-    // Medium
-    static let pretendardMedium18: Font = .custom("Pretendard-Medium", size: 18)
-    static let pretendardMedium16: Font = .custom("Pretendard-Medium", size: 16)
+    static let title2_b_18: Font = .custom("Pretendard-Bold", size: 18)
+    static let title2_sb_18: Font = .custom("Pretendard-SemiBold", size: 18)
+    static let title2_m_18: Font = .custom("Pretendard-Medium", size: 18)
+    static let title2_r_18: Font = .custom("Pretendard-Regular", size: 18)
     
-    // Regular
-    static let pretendardRegular14: Font = .custom("Pretendard-Regular", size: 14)
-    static let pretendardRegular16: Font = .custom("Pretendard-Regular", size: 16)
+    // body
+    static let body1_b_16: Font = .custom("Pretendard-Bold", size: 16)
+    static let body1_sb_16: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let body1_m_16: Font = .custom("Pretendard-Medium", size: 16)
+    static let body1_r_16: Font = .custom("Pretendard-Regular", size: 16)
+    
+    static let body2_b_14: Font = .custom("Pretendard-Bold", size: 14)
+    static let body2_sb_14: Font = .custom("Pretendard-SemiBold", size: 14)
+    static let body2_m_14: Font = .custom("Pretendard-Medium", size: 14)
+    static let body2_r_14: Font = .custom("Pretendard-Regular", size: 14)
+    
+    // caption
+    static let caption1_sb_13: Font = .custom("Pretendard-SemiBold", size: 13)
+    static let caption1_m_13: Font = .custom("Pretendard-Medium", size: 13)
+    static let caption1_r_13: Font = .custom("Pretendard-Regular", size: 13)
+    
+    static let caption1_b_12: Font = .custom("Pretendard-Bold", size: 12)
+    static let caption1_m_12: Font = .custom("Pretendard-Medium", size: 12)
+    static let caption1_r_12: Font = .custom("Pretendard-Regular", size: 12)
     
 }
