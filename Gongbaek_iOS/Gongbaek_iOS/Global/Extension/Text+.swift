4877d2ee6cd2b242267d22d66c635f89f5856161
hee <heheeun13@gmail.com>
2025-01-09 04:54:14 +0900

    [Setting/#1] Text+ 파일 추가. Text에 addFont로 자간, 행간 추가.

---
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Extension/Text+.swift
@@ -0,0 +1,24 @@
+//
+//  Text+.swift
+//  Gongbaek_iOS
+//
+//  Created by 김희은 on 1/9/25.
+//
+
+import SwiftUICore
+
+extension Text {
+    static func addFont(_ text: Text, font: FontFamiliy?) -> some View {
+        let fontName = font?.fontName.rawValue ?? "Pretendard-Regular"
+        let fontSize = font?.fontSize ?? 16
+        let lineHeight = font?.lineHeight ?? 0
+        let letterSpacing = font?.letterSpacing ?? 0
+        
+        let lineSpacingValue = lineHeight - fontSize
+        
+        return text
+            .font(.custom(fontName, size: CGFloat(fontSize)))
+            .tracking(CGFloat(letterSpacing))
+            .lineSpacing(CGFloat(lineSpacingValue))
+    }
+}
