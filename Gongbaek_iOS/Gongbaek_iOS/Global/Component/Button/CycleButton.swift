216ff818094e615ee75b83e1604c881d048bb299
kms0233 <cnblue789@naver.com>
2025-01-12 13:26:31 +0900

    [Feat/#5] CycleButton 컴포넌트 구현

---
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Button/CycleButton.swift
@@ -0,0 +1,39 @@
+//
+//  CycleButton.swift
+//  Gongbaek_iOS
+//
+//  Created by 김민서 on 1/11/25.
+//
+
+import SwiftUI
+
+struct CycleButton: View {
+    var isClicked: Bool = false
+
+    var body: some View {
+        Button(action: {
+            print("주기 버튼 클릭됨")
+        }) {
+            HStack(spacing: 6) {
+                Text(isClicked ? "한번 봐요" : "모든 주기")
+                    .font(.pretendard(.caption1_m_13))
+                    .foregroundColor(isClicked ? .grayWhite : .gray10)
+                //TODO: 아이콘 변경
+                Image(.sample)
+                    .resizable()
+                    .renderingMode(.template)
+                    .foregroundColor(isClicked ? .gray04 : .gray06)
+                    .frame(width: 18, height: 18)
+            }
+            .padding(.horizontal, 12)
+            .padding(.vertical, 6)
+            .background(isClicked ? .gray09: .gray01)
+            .cornerRadius(20)
+        }
+    }
+}
+
+#Preview {
+    CycleButton(isClicked: false)
+}
+
