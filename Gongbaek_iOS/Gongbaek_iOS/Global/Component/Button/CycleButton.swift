c22cfaba1ae84a3679b1d044007378a24ac49455
kms0233 <cnblue789@naver.com>
2025-01-14 23:50:49 +0900

    [Fix/#5] CycleButton 분기처리 수정

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Button/CycleButton.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Button/CycleButton.swift
@@ -7,32 +7,61 @@
 
 import SwiftUI
 
+enum CycleState {
+    case all
+    case weekly
+    case once
+    
+    var titleText: String {
+        switch self {
+        case .all: return "모든 주기"
+        case .weekly: return "매주 봐요"
+        case .once: return "한번만 봐요"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .all: return .gray01
+        case .weekly, .once: return .gray09
+        }
+    }
+    
+    var foregroundColor: Color {
+        switch self {
+        case .all: return .gray10
+        case .weekly, .once: return .grayWhite
+        }
+    }
+}
+
 struct CycleButton: View {
-    var isClicked: Bool = false
+    var state: CycleState
+    var onClick: (() -> Void)?
 
     var body: some View {
         Button(action: {
             print("주기 버튼 클릭됨")
         }) {
             HStack(spacing: 6) {
-                Text(isClicked ? "한번 봐요" : "모든 주기")
+                Text(state.titleText)
                     .font(.pretendard(.caption1_m_13))
-                    .foregroundColor(isClicked ? .grayWhite : .gray10)
+                    .foregroundColor(state.foregroundColor)
                 Image(.icArrowBottom18)
                     .resizable()
                     .renderingMode(.template)
-                    .foregroundColor(isClicked ? .gray04 : .gray06)
+                    .foregroundColor(.gray05)
                     .frame(width: 18, height: 18)
             }
             .padding(.horizontal, 12)
             .padding(.vertical, 6)
-            .background(isClicked ? .gray09: .gray01)
+            .background(state.backgroundColor)
             .cornerRadius(20)
         }
     }
 }
 
 #Preview {
-    CycleButton(isClicked: false)
+    CycleButton(state: .once)
 }
 
