45310e60188ea61f60fdcb1b56d153a37141728c
hee <heheeun13@gmail.com>
2025-01-15 07:41:58 +0900

    [Chore/#23] struct 생성 - 추후 Model 분리

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Bar/ApplyBar.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Bar/ApplyBar.swift
@@ -7,36 +7,39 @@
 
 import SwiftUI
 
-struct ApplyBar: View {
+struct Apply {
     var isActivated: Bool
     var currentPeopleCount: Int
     var maxPeopleCount: Int
     var buttonText: String
+}
+
+struct ApplyBar: View {
+    var apply: Apply
     
     var body: some View {
         HStack(spacing: 16) {
-            Text("\(currentPeopleCount) / \(maxPeopleCount) 명")
+            Text("\(apply.currentPeopleCount) / \(apply.maxPeopleCount) 명")
                 .pretendardFont(.title2_sb_18)
                 .padding(.vertical, 16)
                 .padding(.horizontal, 16)
-                .foregroundStyle(isActivated ? .gray01 : .grayWhite)
-                .background(isActivated ? .gray09 : .gray04)
+                .foregroundStyle(apply.isActivated ? .gray01 : .grayWhite)
+                .background(apply.isActivated ? .gray09 : .gray04)
                 .clipShape(RoundedRectangle(cornerRadius: 6))
             
             Button(action: {
                 print("applyButtonIsTapped")
             }) {
-                Text(buttonText)
+                Text(apply.buttonText)
                     .pretendardFont(.title2_sb_18)
                     .padding(.vertical, 16)
                     .frame(maxWidth: .infinity)
-                    .background(isActivated ? .mainOrange : .gray03)
+                    .background(apply.isActivated ? .mainOrange : .gray03)
                     .foregroundStyle(.grayWhite)
                     .clipShape(RoundedRectangle(cornerRadius: 6))
             }
             .cornerRadius(6)
         }
-        .frame(maxWidth: .infinity)
         .padding(.vertical, 20)
         .padding(.horizontal, 16)
         .background(.grayWhite)
@@ -44,5 +47,5 @@ struct ApplyBar: View {
 }
 
 #Preview {
-    ApplyBar(isActivated: true, currentPeopleCount: 3, maxPeopleCount: 4, buttonText: "엘렐레")
+    ApplyBar(apply: Apply(isActivated: true, currentPeopleCount: 3, maxPeopleCount: 4, buttonText: "엘렐레"))
 }
