fee0ae713e48b7392e89eacc5804c93a5a4f31aa
hee <heheeun13@gmail.com>
2025-01-15 03:57:16 +0900

    [Fix/#13] GeometryReader 제거 및 Stack구조 변경

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/TextField/CommentTextField.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/TextField/CommentTextField.swift
@@ -10,57 +10,44 @@ import SwiftUI
 struct CommentTextField: View {
     @State private var writeComment: String = ""
     @State private var textFieldHeight: CGFloat = 48
-    var sendButtonTapped: (() -> Void)?
+    @State var sendButtonTapped: (() -> Void)?
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            Rectangle()
-                .foregroundStyle(.gray01)
-                .cornerRadius(6)
-                .frame(height: textFieldHeight)
+        HStack(alignment: .bottom, spacing: 4) {
+            TextField(
+                "댓글을 입력해주세요.",
+                text: $writeComment,
+                axis: .vertical
+            )
+            .pretendardFont(.body1_m_16)
+            .background(.gray01)
+            .foregroundColor(.gray10)
+            .lineLimit(1...3)
+            .padding(.vertical, 14)
+            .padding(.leading, 12)
             
-            HStack(alignment: .bottom, spacing: 0) {
-                TextField(
-                    "댓글을 입력해주세요.",
-                    text: $writeComment,
-                    axis: .vertical
-                )
-                .pretendardFont(.body1_m_16)
-                .background(.gray01)
-                .foregroundColor(.gray10)
-                
-                .lineLimit(1...3)
-                
-                .padding(.vertical, 14)
-                .padding(.leading, 12)
-                .padding(.trailing, 4)
-                .background(
-                    GeometryReader { geometry in
-                        Color.clear
-                            .onChange(of: geometry.size.height) { oldHeight, newHeight in
-                                self.textFieldHeight = newHeight
-                            }
-                    }
-                )
-                
-                Button(action: {
-                    sendButtonTapped?() // 깡통
-                    guard !writeComment.isEmpty else { return }
-                    writeComment = ""
-                }) {
-                    Image("ic_send_24")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                }
-                .padding(.bottom, 14)
-                .padding(.trailing, 12)
-                .frame(alignment: .bottom)
-                .foregroundStyle(.mainOrange)
+            Button(action: {
+                sendButtonTapped?() // 깡통
+                guard !writeComment.isEmpty else { return }
+                writeComment = ""
+            }) {
+                Image("ic_send_24")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
             }
+            .padding(.bottom, 12)
+            .padding(.trailing, 12)
+            .foregroundStyle(.mainOrange)
+            .frame(alignment: .bottomTrailing)
         }
+        .background(
+            RoundedRectangle(cornerRadius: 6)
+                .fill(.gray01)
+        )
         .padding(.vertical, 10)
         .padding(.horizontal, 16)
+        .background(.grayWhite)
     }
 }
 
