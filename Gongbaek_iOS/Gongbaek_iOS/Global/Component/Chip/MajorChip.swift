f60b3f67990e664795b0dd43636731b4fbcedf28
hee <heheeun13@gmail.com>
2025-01-15 01:38:21 +0900

    [Feat/#13] grayscale MajorChip 생성

---
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Chip/MajorChip.swift
@@ -0,0 +1,24 @@
+//
+//  MajorChip.swift
+//  Gongbaek_iOS
+//
+//  Created by 김희은 on 1/15/25.
+//
+
+import SwiftUI
+
+struct MajorChip: View {
+    let major: String
+    
+    var body: some View {
+        Text(major)
+            .pretendardFont(.caption2_m_12)
+            .foregroundStyle(.grayWhite)
+            .padding(.horizontal, 6)
+            .padding(.vertical, 1)
+            .background(
+                RoundedRectangle(cornerRadius: 4)
+                    .fill(.gray09)
+            )
+    }
+}
