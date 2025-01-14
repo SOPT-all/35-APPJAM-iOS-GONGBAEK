e2b648fdf86fa87ad7246ad383aac427e9c56146
kms0233 <cnblue789@naver.com>
2025-01-14 23:21:13 +0900

    [Feat/#11] GroupChip UI 구현

--- Gongbaek_iOS/Gongbaek_iOS/Global/GroupChip.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/GroupChip.swift
@@ -72,4 +72,63 @@ extension GroupChipState {
         }
     }
 }
+
+struct GroupChip: View {
+    var state: GroupChipState
+    
+    var body: some View {
+        Text(state.titleText)
+            .font(.pretendard(.caption2_r_12))
+            .foregroundColor(state.textColor)
+            .padding(.horizontal, 4)
+            .padding(.vertical, 1)
+            .padding(.horizontal, 4)
+            .background(state.backgroundColor)
+            .cornerRadius(2)
+    }
 }
+
+#Preview {
+    GroupChip(state: .recruiting(.recruiting))
+    GroupChip(state: .recruiting(.recruited))
+    GroupChip(state: .recruiting(.closed))
+    GroupChip(state: .category(.play))
+    GroupChip(state: .category(.networking))
+    GroupChip(state: .weekly(true))
+    GroupChip(state: .weekly(false))
+    
+}
+
+//TODO: #5번 이슈 머지되면 삭제
+enum CategoryImageState: Int, CaseIterable {
+    case study = 1
+    case dining = 2
+    case sport = 3
+    case networking = 4
+    case play = 5
+    case others = 6
+    
+    //TODO: 일러스트 이미지 추가
+    var categoryImage: String {
+        switch self {
+        case .study: return "sample"
+        case .dining: return ""
+        case .sport: return ""
+        case .networking: return ""
+        case .play: return ""
+        case .others: return ""
+        }
+    }
+    
+    var catogoryName: String {
+        switch self {
+        case .study: return "스터디"
+        case .dining: return "식사"
+        case .sport: return "운동/산책"
+        case .networking: return "네트워킹"
+        case .play: return "놀기"
+        case .others: return "기타"
+        }
+    }
+}
+
