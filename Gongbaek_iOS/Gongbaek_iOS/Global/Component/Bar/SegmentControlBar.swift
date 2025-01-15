65294799733f64579da190a669f35e925d5688d4
hee <heheeun13@gmail.com>
2025-01-15 20:51:46 +0900

    [Feat/#24] SegmentControlBar 생성 및 CustomSegmentControl 삽입

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Bar/SegmentControlBar.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Bar/SegmentControlBar.swift
@@ -1,7 +1,56 @@
 //
-//  SegmentControl.swift
+//  SegmentControlBar.swift
 //  Gongbaek_iOS
 //
 //  Created by 김희은 on 1/15/25.
 //
 
+import SwiftUI
+
+struct SegmentControlBar: View {
+    @State var segmentTitles: [String]
+    @State var selectedIndex = 0
+    
+    var body: some View {
+        VStack {
+            CustomSegmentControl(segments: segmentTitles, selected: $selectedIndex)
+            Spacer()
+        }
+        .background(.gray05)
+    }
+}
+
+struct CustomSegmentControl: View {
+    let segments: [String]
+    @Binding var selected: Int
+
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(segments.indices, id: \.self) { index in
+                Button {
+                    selected = index
+                } label: {
+                    ZStack(alignment: .bottom) {
+                        Text(segments[index])
+                            .pretendardFont(.body1_m_16)
+                            .foregroundColor(selected == index ? .gray10 : .gray05)
+                            .padding(.vertical, 15)
+                        
+                        ZStack(alignment: .bottom) {
+                            Color(.gray02)
+                                .frame(height: 2)
+                            selected == index ? Color(.gray09).frame(height: 4) : nil
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .background(.grayWhite)
+            }
+        }
+    }
+}
+
+#Preview {
+    SegmentControlBar(segmentTitles: ["모임정보", "댓글"])
+    SegmentControlBar(segmentTitles: ["내가 모집한", "내가 신청한"])
+}
