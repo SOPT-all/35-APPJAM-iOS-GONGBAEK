48ed36c565d1bf2410ee223a324c0388dbe8cabe
hee <heheeun13@gmail.com>
2025-01-15 22:21:14 +0900

    [Fix/#26] Struct 로 OwnerProfile에 들어갈 정보 분리

--- Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/OwnerProfileBox.swift
+++ Gongbaek_iOS/Gongbaek_iOS/Global/Component/Box/OwnerProfileBox.swift
@@ -7,7 +7,9 @@
 
 import SwiftUI
 
-struct OwnerProfileBox: View {
+// MARK: - TODO: Model로 분리
+
+struct OwnerProfileData {
     var profileImage: Int
     var nickname: String
     var sex: String
@@ -15,6 +17,10 @@ struct OwnerProfileBox: View {
     var enterYear: Int
     var schoolGrade: Int
     var mbti: String
+}
+
+struct OwnerProfileBox: View {
+    let ownerData: OwnerProfileData
     
     var body: some View {
         ZStack(alignment: .leading) {
@@ -25,31 +31,31 @@ struct OwnerProfileBox: View {
                 Image(systemName: "person")
                     .resizable()
                     .frame(width: 80, height: 80)
-                    .background(.gray04) // MARK: - 이미지 삽입 시 삭제
-                    .foregroundStyle(.mainOrange) // MARK: - 이미지 삽입 시 삭제
+                    .background(.gray04) // MARK: - TODO: 이미지 삽입 시 삭제
+                    .foregroundStyle(.mainOrange) // MARK: - TODO: 이미지 삽입 시 삭제
                 
                 VStack(alignment: .leading, spacing: 6) {
                     HStack(spacing: 6) {
-                        Text(nickname)
+                        Text(ownerData.nickname)
                             .pretendardFont(.body1_b_16)
                             .foregroundStyle(.grayBlack)
                         
-                        Image(sex == "MAN" ? .icMale20 : .icFemale20)
+                        Image(ownerData.sex == "MAN" ? .icMale20 : .icFemale20)
                             .resizable()
                             .frame(width: 20, height: 20)
                             .foregroundStyle(.gray06)
                     }
                     
-                    MajorChip(major: schoolMajor, targetObject: .ownerProfile)
+                    MajorChip(major: ownerData.schoolMajor, targetObject: .ownerProfile)
                     
                     HStack(spacing: 6) {
                         ProfileDetailChip(detailCategory: "학번/학년")
-                        Text("\(enterYear%100)학번 \(schoolGrade)학년")
+                        Text("\(ownerData.enterYear%100)학번 \(ownerData.schoolGrade)학년")
                             .foregroundStyle(.gray08)
                             .pretendardFont(.caption2_m_12)
                             .padding(.trailing, 6)
                         ProfileDetailChip(detailCategory: "MBTI")
-                        Text(mbti)
+                        Text(ownerData.mbti)
                             .foregroundStyle(.gray08)
                             .pretendardFont(.caption2_m_12)
                     }
@@ -61,5 +67,5 @@ struct OwnerProfileBox: View {
 }
 
 #Preview {
-    OwnerProfileBox(profileImage: 1, nickname: "로이임탄", sex: "MALE", schoolMajor: "글로벌문화산업/MICE 전공", enterYear: 2019, schoolGrade: 4, mbti: "ENFJ")
+    OwnerProfileBox(ownerData: OwnerProfileData(profileImage: 1, nickname: "로이임탄", sex: "MALE", schoolMajor: "글로벌문화산업/MICE 전공", enterYear: 2019, schoolGrade: 4, mbti: "ENFJ"))
 }
